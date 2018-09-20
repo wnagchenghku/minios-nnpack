4aad216fc65b7087d935d519680a8a026e82f408
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-09-20 16:04:09 +0200

    revert nnpack; ket's build with libc

--- NNPACK/src/init.c
+++ NNPACK/src/init.c
@@ -4,7 +4,7 @@
 
 #include <pthread.h>
 
-#include <cpuinfo.h>
+//#include <cpuinfo.h>
 
 #include <nnpack.h>
 #include <nnpack/hwinfo.h>
@@ -599,9 +599,9 @@ static void init_hwinfo(void) {
 }
 
 enum nnp_status nnp_initialize(void) {
-	if (!cpuinfo_initialize()) {
-		return nnp_status_out_of_memory;
-	}
+	// if (!cpuinfo_initialize()) {
+	// 	return nnp_status_out_of_memory;
+	// }
 	pthread_once(&hwinfo_init_control, &init_hwinfo);
 	if (nnp_hwinfo.supported) {
 		return nnp_status_success;
@@ -611,6 +611,6 @@ enum nnp_status nnp_initialize(void) {
 }
 
 enum nnp_status nnp_deinitialize(void) {
-	cpuinfo_deinitialize();
+	// cpuinfo_deinitialize();
 	return nnp_status_success;
 }
